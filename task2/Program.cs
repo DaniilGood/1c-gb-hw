fc6cc74e9a33cc27bb34a8e8e6bff3fa79c5bfe2
Daniil <daniil.gudkows@gmail.com>
2022-12-03 18:43:55 +0300

    финальное дз задачи 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3 
+
+Console.WriteLine("Введите два числа: ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.WriteLine(num1 + " больше " + num2);
+}
+else
+{
+    Console.WriteLine(num2 + " больше " + num1 );
+}
