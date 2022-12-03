30555b97cb95b3f32feb4098519bc81008bf191d
Daniil <daniil.gudkows@gmail.com>
2022-12-03 22:01:12 +0300

    hw6 is done

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int a = num% 2;
+
+if(a == 0)
+{
+    Console.WriteLine(num+ "-> чет");
+}
+else
+{
+    Console.WriteLine(num+ "-> нечет");
+}
