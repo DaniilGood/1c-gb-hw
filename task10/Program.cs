9e5c9fcd8c332da1ee824005b6cd366ed8367538
Daniil <daniil.gudkows@gmail.com>
2022-12-04 23:35:29 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите 3х значное число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if(number <= 999 && number >= 100)
+{
+    number= (number-number%10)%100/10;
+    Console.WriteLine("Вторая цифра числа ->"+ number);
+}
+else
+{
+    Console.WriteLine("Число не верного размера");
+}
